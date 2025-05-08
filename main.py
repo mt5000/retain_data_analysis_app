b9118aeb75111f7397f4a2c93d4c80c5d7c51253
mt5000 <matt@inclusively.com>
2025-05-08 17:22:01 -0400

    Add LLM integration and update dependencies
    Integrated Google Gemini LLM for AI-driven content generation and added a new function `get_llm_result`. Updated project dependencies in `pyproject.toml` to include required packages for Streamlit, BigQuery, and Google GenAI.

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import os
 
 import streamlit as st
 import pandas as pd
+from google.genai import Client as GeminiClient
+from google.genai.types import GenerateContentConfig, Tool, ToolCodeExecution
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
 
+gemini = GeminiClient()
+
 def get_bigquery_table(
-    project_id: str = "healthy-dragon-300820",
-    dataset_id: str = "application_analytics",
-    table_id: str = "retain_for_ai_analysis",
-):
+                        project_id: str = "healthy-dragon-300820",
+                        dataset_id: str = "application_analytics",
+                        table_id: str = "retain_for_ai_analysis",
+                       ) -> pd.DataFrame | None:
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_credentials"]
     )
@@ -25,6 +29,15 @@ def get_bigquery_table(
         st.write(e)
         return None
 
+
+def get_llm_result(query: str):
+    ai_response = gemini.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[query,
+        ],
+        config=GenerateContentConfig(tools=[Tool(code_execution=ToolCodeExecution)])
+    )
+    return ai_response
 
 st.markdown("<div class='title'>Success Enabler Search & Discovery Feedback Form</div>", unsafe_allow_html=True)
 
